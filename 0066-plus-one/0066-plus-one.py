ae0eb949d32734d347d9a2a193a2d1764f9f415e
Tayyab Ali <ta185381@gmail.com>
2026-07-11 20:16:27 +0530

    Time: 0 ms (100%), Space: 19.3 MB (55.93%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,6 +1,7 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         # Approach 1
+        '''
         n = len(digits)
         l = 0
         for i in digits:
@@ -10,3 +11,13 @@ class Solution:
         for i in app:
             res.append(int(i))
         return res
+        '''
+        # Approach 2
+        n= len(digits)
+        for i in range(n-1,-1,-1):
+            if digits[i]<9:
+                digits[i]+=1
+                return digits
+            else:
+                digits[i]=0
+        return [1]+digits
