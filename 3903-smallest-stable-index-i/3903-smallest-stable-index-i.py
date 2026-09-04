fbbbb2c190092f6484e240f108d194d1238bd82b
Tayyab Ali <ta185381@gmail.com>
2026-09-04 12:41:15 +0530

    Time: 4 ms (58.13%), Space: 19.3 MB (71.43%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -2,11 +2,13 @@ class Solution:
     def firstStableIndex(self, nums1: list[int], k: int) -> int:
 
         # Approach - 1
+        '''
         sm = []
         for i in range(len(nums1)):
             if max(nums1[0:i+1])-min(nums1[i:])<=k:
                 return i
         return -1
+        '''
         
         # Approach - 2
 
@@ -19,7 +21,8 @@ class Solution:
         for i in range(1,n):
             pref[i] = max(pref[i-1],nums1[i])
         for i in range(n-2,-1,-1):
-            suff[i] = min(suff[i+1],nums[i])
+            suff[i] = min(suff[i+1],nums1[i])
         for i in range(n):
             if pref[i]-suff[i]<=k:
                 return i
+        return -1
