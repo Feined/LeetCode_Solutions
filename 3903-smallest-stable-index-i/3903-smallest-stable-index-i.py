916ad79b5dc04f6ca12a97a202f00d66da130d08
Tayyab Ali <ta185381@gmail.com>
2026-09-04 12:40:23 +0530

    Time: 7 ms (48.03%), Space: 19.3 MB (71.43%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -8,3 +8,18 @@ class Solution:
                 return i
         return -1
         
+        # Approach - 2
+
+        n = len(nums1)
+        pref = [0]*n
+        suff = [0]*n
+
+        pref[0] = nums1[0]
+        suff[n-1] = nums1[n-1]
+        for i in range(1,n):
+            pref[i] = max(pref[i-1],nums1[i])
+        for i in range(n-2,-1,-1):
+            suff[i] = min(suff[i+1],nums[i])
+        for i in range(n):
+            if pref[i]-suff[i]<=k:
+                return i
