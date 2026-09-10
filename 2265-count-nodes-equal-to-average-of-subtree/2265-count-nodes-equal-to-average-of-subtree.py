f9a60911c234cc12c8ca9ada1b5acb19550e1a85
Tayyab Ali <ta185381@gmail.com>
2026-09-10 23:55:15 +0530

    Time: 42 ms (94.04%), Space: 19.6 MB (69.46%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
@@ -1,4 +1,5 @@
 class Solution:
+    ''' Approach - 1
     def findsum(self,root,cnt):
         if root is None:
             return 0
@@ -15,9 +16,28 @@ class Solution:
             self.res+=1
         self.solve(root.left)
         self.solve(root.right)
+    '''
+    # Approach - 2
     def averageOfSubtree(self, root: TreeNode) -> int:
-        self.res=0
-        self.solve(root)
-        return self.res
+        # self.res=0
+        # self.solve(root)
+        # return self.res
+        count = 0
+        def dfs(root):
+            nonlocal count
+            if not root:
+                return 0,0
+            lsum,lcnt = dfs(root.left)
+            rsum,rcnt = dfs(root.right)
+
+            totalsum = lsum+rsum+root.val
+            totalcnt = lcnt+rcnt+1
+
+            if totalsum//totalcnt == root.val:
+                count+=1
+            return totalsum,totalcnt
+        dfs(root)
+        return count
+
         
         
